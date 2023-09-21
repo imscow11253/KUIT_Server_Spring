ef0ab914003aa0d13f3a562e158d856da957e375
KMH <seaweed8759@naver.com>
2023-09-21 15:45:27 +0900

    feat : changing code to 1week/completed

---
+++ src/main/java/ladder/creator/LadderCreator.java
@@ -0,0 +1,26 @@
+package ladder.creator;
+
+import ladder.NaturalNumber;
+import ladder.Position;
+import ladder.Row;
+
+public class LadderCreator {
+
+    private final Row[] rows;
+
+    public LadderCreator(NaturalNumber numberOfRow, NaturalNumber numberOfPerson) {
+        rows = new Row[numberOfRow.getNumber()];
+        for (int i = 0; i < numberOfRow.getNumber(); i++) {
+            rows[i] = new Row(numberOfPerson);
+        }
+    }
+
+
+    public void drawLine(Position row, Position col) {
+        rows[row.getValue()].drawLine(col);
+    }
+
+    public Row[] getRows() {
+        return rows;
+    }
+}
