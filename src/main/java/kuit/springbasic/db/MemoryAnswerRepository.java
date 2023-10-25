11cf2c55957236e05fbe811f710d4d6a3b78cdcc
Han Ju Kim <hjkim2902@gmail.com>
2023-10-25 21:48:17 +0900

    feat : add Repository templates

---
+++ src/main/java/kuit/springbasic/db/MemoryAnswerRepository.java
@@ -0,0 +1,50 @@
+package kuit.springbasic.db;
+
+
+import kuit.springbasic.web.domain.Answer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Repository
+public class MemoryAnswerRepository {
+
+    private Map<String, Answer> answers= new HashMap<>();
+    private  static int PK = 0;
+
+    public MemoryAnswerRepository() {
+        insert(new Answer(1,"함형주","7호선 타세요"));
+    }
+
+    public int getPK(){
+        return ++PK;
+    }
+    public List<Answer> findAllByQuestionId(int id) {
+        ArrayList<Answer> result = new ArrayList<>();
+
+        Set<String> answerKeys = answers.keySet();
+        for (String key : answerKeys) {
+            Answer repoAnswer = answers.get(key);
+            if(repoAnswer.getQuestionId() == id){
+                result.add(repoAnswer);
+            }
+        }
+
+        return result;
+    }
+
+
+    public Answer insert(Answer answer) {
+        answer.setAnswerId(getPK());
+        answer.setCreatedDate(Date.valueOf(LocalDate.now()));
+        answers.put(Integer.toString(answer.getAnswerId()),answer);
+        return answer;
+    }
+}
---
+++ src/main/java/kuit/springbasic/db/MemoryQuestionRepository.java
@@ -0,0 +1,48 @@
+package kuit.springbasic.db;
+
+
+import kuit.springbasic.web.domain.Question;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class MemoryQuestionRepository {
+    private Map<String, Question> questions= new HashMap<>();
+    private static int PK = 0;
+
+    public MemoryQuestionRepository() {
+        insert(new Question("이주언","건대 어떻게 가나요?","홍대에서 건대 어떻게 가나요?",1));
+        insert(new Question("강연주","서버 개잘하는데 질문받음","질문 ㄱ",0));
+
+    }
+
+    public static int getPK(){
+        return ++PK;
+    }
+
+    public Question findByQuestionId(int id) {
+        return questions.get(String.valueOf(id));
+    }
+
+    public void update(Question question) {
+        Question repoQuestion = questions.get(Integer.toString(question.getQuestionId()));
+        repoQuestion.update(question);
+    }
+
+    public void insert(Question question) {
+        question.setQuestionId(getPK());
+        questions.put(Integer.toString(question.getQuestionId()), question);
+    }
+
+    public void updateCountOfAnswer(Question question) {
+        Question repoQuestion = questions.get(Integer.toString(question.getQuestionId()));
+        repoQuestion.updateCountofAnswer(question);
+    }
+
+    public List<Question> findAll() {
+        return questions.values().stream().toList();
+    }
+}
