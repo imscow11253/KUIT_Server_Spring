62c9d2722f069539d2e4037cabeb6e94ecdb3ffc
Han Ju Kim <hjkim2902@gmail.com>
2023-10-25 21:46:44 +0900

    feat : add UserSessionUtils

---
+++ src/main/java/kuit/springbasic/web/util/UserSessionUtils.java
@@ -0,0 +1,21 @@
+package kuit.springbasic.web.util;
+
+
+import jakarta.servlet.http.HttpSession;
+import kuit.springbasic.web.domain.User;
+
+public class UserSessionUtils {
+    public static final String USER_SESSION_KEY = "user";
+
+    public static User getUserFromSession(HttpSession session) {
+        Object user = session.getAttribute(USER_SESSION_KEY);
+        if (user == null) {
+            return null;
+        }
+        return (User) user;
+    }
+
+    public static boolean isLoggedIn(HttpSession session) {
+        return getUserFromSession(session) != null;
+    }
+}
